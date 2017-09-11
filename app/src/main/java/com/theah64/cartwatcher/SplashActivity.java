e9a7970f42c55de0257ac9b05244fba8dafdeddc
theapache64-charlie <theapache64@gmail.com>
2017-09-11 21:00:41 +0530

    Added splash

---
+++ app/src/main/java/com/theah64/cartwatcher/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.theah64.cartwatcher;
+
+import com.theah64.retrokit.activities.BaseSplashActivity;
+
+/**
+ * Created by theapache64 on 11/9/17.
+ */
+
+public class SplashActivity extends BaseSplashActivity {
+
+    @Override
+    public void onSplashStart() {
+
+    }
+
+    @Override
+    public long getSplashDuration() {
+        return 2000;
+    }
+
+    @Override
+    public void onSplashFinished() {
+
+    }
+
+    @Override
+    public int getLogo() {
+        return 0;
+    }
+}
---
+++ retrokit/src/main/java/com/theah64/retrokit/activities/BaseSplashActivity.java
@@ -0,0 +1,43 @@
+package com.theah64.retrokit.activities;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.DrawableRes;
+import android.widget.ImageView;
+
+import com.theah64.retrokit.R;
+
+
+/**
+ * Created by theapache64 on 20/8/17.
+ */
+
+public abstract class BaseSplashActivity extends BaseAppCompatActivity {
+
+    public abstract void onSplashStart();
+
+    public abstract long getSplashDuration();
+
+    public abstract void onSplashFinished();
+
+    @DrawableRes
+    public abstract int getLogo();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ((ImageView) findViewById(R.id.ivSplashLogo)).setImageResource(getLogo());
+
+        onSplashStart();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onSplashFinished();
+                finish();
+            }
+        }, getSplashDuration());
+
+    }
+}
