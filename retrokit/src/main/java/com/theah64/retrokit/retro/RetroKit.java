19de0d42d962d240f87465a53d11b852dce42f44
theapache64-charlie <theapache64@gmail.com>
2017-09-11 20:07:55 +0530

    Integrated 'RetroKit' local

---
+++ retrokit/src/main/java/com/theah64/retrokit/retro/RetroKit.java
@@ -0,0 +1,88 @@
+package com.theah64.retrokit.retro;
+
+import com.joanzapata.iconify.IconFontDescriptor;
+import com.joanzapata.iconify.Iconify;
+import com.theah64.retrokit.R;
+import com.wang.avi.Indicator;
+import com.wang.avi.indicators.BallPulseSyncIndicator;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
+/**
+ * Created by theapache64 on 25/8/17.
+ */
+
+public class RetroKit {
+
+    private static final RetroKit instance = new RetroKit();
+
+    private static final String FONT_ROBOTO_REGULAR = "fonts/Roboto-Regular.ttf";
+    private static final String FONT_ROBOTO_BOLD = "fonts/Roboto-Bold.ttf";
+    private static final String FONT_ROBOTO_MEDIUM = "fonts/Roboto-Medium.ttf";
+    public static final String FONT_PACIFICO = "fonts/Pacifico.ttf";
+
+    private Indicator defaultProgressIndicator;
+    private int defaultProgressIndicatorColor;
+    private boolean isDebug;
+
+    public static RetroKit getInstance() {
+        return instance;
+    }
+
+    private RetroKit() {
+        this.defaultProgressIndicator = new BallPulseSyncIndicator();
+        this.defaultProgressIndicatorColor = android.support.design.R.attr.colorPrimary;
+        this.isDebug = false;
+    }
+
+    public boolean isDebug() {
+        return isDebug;
+    }
+
+    public RetroKit setDebug(boolean debug) {
+        isDebug = debug;
+        return this;
+    }
+
+    public RetroKit setRetrofitBaseURL(String baseUrl) {
+        RetrofitClient.setBaseUrl(baseUrl);
+        return this;
+    }
+
+    public RetroKit setIconModule(IconFontDescriptor descriptor) {
+        Iconify.with(descriptor);
+        return this;
+    }
+
+    public RetroKit setDefaultProgressIndicator(Indicator defaultProgressIndicator) {
+        this.defaultProgressIndicator = defaultProgressIndicator;
+        return this;
+    }
+
+    public RetroKit setDefaultProgressIndicatorColor(int defaultProgressIndicatorColor) {
+        this.defaultProgressIndicatorColor = defaultProgressIndicatorColor;
+        return this;
+    }
+
+    public Indicator getDefaultProgressIndicator() {
+        return defaultProgressIndicator;
+    }
+
+    public int getDefaultProgressIndicatorColor() {
+        return defaultProgressIndicatorColor;
+    }
+
+    public RetroKit setDefaultFontPathAsRobotoRegular() {
+        setDefaultFontPath(FONT_ROBOTO_REGULAR);
+        return this;
+    }
+
+    public RetroKit setDefaultFontPath(String defaultAppFont) {
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath(defaultAppFont)
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
+        return this;
+    }
+}
---
+++ retrokit/src/main/java/com/theah64/retrokit/retro/RetrofitClient.java
@@ -0,0 +1,38 @@
+package com.theah64.retrokit.retro;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by theapache64 on 25/8/17.
+ */
+
+public class RetrofitClient {
+
+    //private static final String BASE_URL = "http://theapache64.xyz:8080/mock_api/get_json/retrokitexample/";
+    private static Retrofit retrofit = null;
+
+    private static String baseUrl;
+
+    public static void setBaseUrl(String baseUrl) {
+        RetrofitClient.baseUrl = baseUrl;
+    }
+
+    public static Retrofit getClient() {
+
+        if (baseUrl == null) {
+            throw new IllegalArgumentException("Call RetroKit.init() from your Application class");
+        }
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit;
+    }
+
+
+}
