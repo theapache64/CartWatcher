db0cf618ecf4f090761c2b0460a913b82b145979
theapache64-alpha <theapache64@gmail.com>
2017-09-15 08:45:55 +0530

    .backup

--- android/app/src/main/java/com/theah64/cartwatcher/activities/AddProductActivity.java
+++ android/app/src/main/java/com/theah64/cartwatcher/activities/AddProductActivity.java
@@ -2,19 +2,35 @@ package com.theah64.cartwatcher.activities;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.theah64.cartwatcher.R;
+import com.theah64.cartwatcher.widgets.ValidTextInputLayout;
 import com.theah64.retrokit.activities.BaseAppCompatActivity;
 
+import butterknife.BindView;
+
 public class AddProductActivity extends BaseAppCompatActivity {
+
+    @BindView(R.id.vtilProductURL)
+    ValidTextInputLayout vtilProductURL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_product);
+        setContentViewWithButterKnife(R.layout.activity_add_product);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        enableBackNavigation();
 
+        final ValidTextInputLayout.InputValidator inputValidator = new ValidTextInputLayout.InputValidator(vtilProductURL);
+
+        if (inputValidator.isAllValid()) {
+            Toast.makeText(this, "Valid", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
--- android/app/src/main/java/com/theah64/cartwatcher/activities/MainActivity.java
+++ android/app/src/main/java/com/theah64/cartwatcher/activities/MainActivity.java
@@ -3,26 +3,15 @@ package com.theah64.cartwatcher.activities;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.Toolbar;
-import android.text.InputType;
 import android.view.View;
-import android.webkit.URLUtil;
-import android.widget.Toast;
 
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.theah64.cartwatcher.R;
 import com.theah64.cartwatcher.database.Products;
-import com.theah64.cartwatcher.exceptions.CartWatcherSQLException;
-import com.theah64.cartwatcher.models.Product;
-import com.theah64.cartwatcher.responses.GetProductResponse;
-import com.theah64.cartwatcher.utils.APIInterface;
 import com.theah64.retrokit.activities.BaseAppCompatActivity;
-import com.theah64.retrokit.retro.BaseAPIResponse;
-import com.theah64.retrokit.retro.CustomRetrofitCallback;
-import com.theah64.retrokit.retro.RetrofitClient;
 
 public class MainActivity extends BaseAppCompatActivity {
 
@@ -39,63 +28,7 @@ public class MainActivity extends BaseAppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                final String lastClipboardData = getValidRecentProductURLFromClipboard();
-
-                //Getting url
-                new MaterialDialog.Builder(MainActivity.this)
-                        .title(R.string.Add_product)
-                        .inputType(InputType.TYPE_CLASS_TEXT)
-                        .autoDismiss(false)
-                        .input(getString(R.string.Paste_product_URL_here), lastClipboardData, false, new MaterialDialog.InputCallback() {
-                            @Override
-                            public void onInput(@NonNull final MaterialDialog dialog, CharSequence input) {
-
-                                final String productUrl = input.toString();
-
-                                if (URLUtil.isValidUrl(productUrl)) {
-
-
-                                    //Valid url
-                                    RetrofitClient.getClient().create(APIInterface.class).getProduct(productUrl).enqueue(new CustomRetrofitCallback<BaseAPIResponse<GetProductResponse>, GetProductResponse>() {
-                                        @Override
-                                        protected void onSuccess(GetProductResponse data) {
-                                            System.out.println("Product loaded: " + data.getProduct());
-                                            dialog.dismiss();
-
-                                            final Product product = data.getProduct();
-
-
-                                            if (!products.exist(product)) {
-
-                                                //Ask interval and interval type
-                                                
-
-                                                try {
-                                                    products.add(product);
-                                                } catch (CartWatcherSQLException e) {
-                                                    e.printStackTrace();
-                                                }
-                                            } else {
-                                                //Product exist in db
-                                                Toast.makeText(MainActivity.this, R.string.Product_exists, Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-
-                                        @Override
-                                        protected void onFailure(String message) {
-                                            System.out.println("Product failed to load: " + message);
-                                        }
-                                    });
-
-                                } else {
-                                    dialog.dismiss();
-                                    Toast.makeText(MainActivity.this, R.string.Invalid_URL, Toast.LENGTH_SHORT).show();
-                                }
-                            }
-                        })
-                        .build().show();
-
+                startActivity(new Intent(MainActivity.this, AddProductActivity.class));
             }
         });
 
